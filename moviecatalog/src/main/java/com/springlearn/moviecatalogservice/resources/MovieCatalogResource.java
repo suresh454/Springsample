bdb78f963af51e033be96061767cfd7d5eb3f25f
Suresh Kothakonda <skothakonda@aligntech.com>
2022-02-20 13:01:59 -0600

    added eureka server and clinet

--- moviecatalog/src/main/java/com/springlearn/moviecatalogservice/resources/MovieCatalogResource.java
+++ moviecatalog/src/main/java/com/springlearn/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,23 +1,52 @@
 package com.springlearn.moviecatalogservice.resources;
 
 import com.springlearn.moviecatalogservice.model.CatalogItem;
+import com.springlearn.moviecatalogservice.model.Movie;
+import com.springlearn.moviecatalogservice.model.Rating;
+import com.springlearn.moviecatalogservice.model.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Test", 4)
-        );
+        //get all movie rating associated with user
+        UserRating ratings = restTemplate.getForObject("http://rating-data-service/ratingsdata/users/"+userId, UserRating.class);
+
+        //For each movie ID, call movie info service and get all movie info
+
+        //put them all together
+
+        return ratings.getRatings().stream().map(rating -> {
+            // Using rest template
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+ rating.getMovieId(), Movie.class);
+                   //Using webclient
+              /*Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+                            .retrieve().bodyToMono(Movie.class).block();*/
+              return  new CatalogItem(movie.getName(), "test", rating.getRating());
+        }
+        ).collect(Collectors.toList());
+
+
     }
 }
--- ratingdata/src/main/java/com/springlearn/ratingdataservice/resources/RatingResource.java
+++ ratingdata/src/main/java/com/springlearn/ratingdataservice/resources/RatingResource.java
@@ -1,10 +1,14 @@
 package com.springlearn.ratingdataservice.resources;
 
 import com.springlearn.ratingdataservice.models.Rating;
+import com.springlearn.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -13,5 +17,16 @@ public class RatingResource {
     @GetMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getRatingforUser(@PathVariable("userId") String userId){
+         List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setRatings(ratings);
+        return  userRating;
     }
 }
